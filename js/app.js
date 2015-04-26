7e5af7f75501414e93006704d57c2da2c3913568
Andres Rojas <breakyboy@gmail.com>
2015-04-26 01:54:54 -0400

    Enemy can now change lanes on every re-spawn - Moved instantiation logic to Enemy.spawn() method

--- js/app.js
+++ js/app.js
@@ -13,15 +13,14 @@ var GRID = {
 };
 
 // Enemies our player must avoid
-var Enemy = function(pos_y) {
+var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    this.x = -100;
-    this.set_lane();
+    this.spawn();
 };
 
 // Update the enemy's position, required method for game
@@ -30,7 +29,10 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x = (this.x > 505) ? -100 : this.x + (60 * dt);
+    if (this.x > 505)
+        this.spawn();
+    else
+        this.x = this.x + (60 * dt);
 };
 
 // Draw the enemy on the screen, required method for game
@@ -40,6 +42,11 @@ Enemy.prototype.render = function() {
 
 Enemy.prototype.set_lane = function() {
     this.y = Math.floor(Math.random() * (3)) + 2;
+};
+
+Enemy.prototype.spawn = function() {
+    this.x = -100;
+    this.set_lane();
 };
 
 // Now write your own player class
@@ -94,7 +101,7 @@ Player.prototype.reset = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var allEnemies = [new Enemy(2)];
+var allEnemies = [new Enemy()];
 var player = new Player();
 
 // This listens for key presses and sends the keys to your

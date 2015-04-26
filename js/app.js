fa4652b5e2d8dca9a565238f91a348ba546991f5
Andres Rojas <breakyboy@gmail.com>
2015-04-26 01:40:54 -0400

    Enemy movement

--- js/app.js
+++ js/app.js
@@ -30,6 +30,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = (this.x > 505) ? -100 : this.x + (60 * dt);
 };
 
 // Draw the enemy on the screen, required method for game

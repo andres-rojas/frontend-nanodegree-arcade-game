a91e70ee28174365494012e2b7d563226e1e7c48
Andres Rojas <breakyboy@gmail.com>
2015-04-26 03:28:32 -0400

    Programmatically construct GRID

--- js/globals.js
+++ js/globals.js
@@ -1,13 +1,36 @@
-// Representative grid for movement
-// Uses traditional x/y plotting
-// i.e: y  ____________________
-//      4 |___|___|___|___|___|
-//      3 |___|___|___|___|___|
-//      2 |___|___|___|___|___|
-//      1 |___|___|___|___|___|
-//      0 |___|___|___|___|___|
-//          0   1   2   3   4   x
-var GRID = {
-    x: [0, 101, 202, 303, 404],
-    y: [400, 317, 234, 151, 68]
+// Cell sizes for the grid
+var CELL = {
+    width: 101,
+    height: 83
 };
+
+// Representative grid for movement
+// width:    number of cells wide
+// height:   number of cells tall
+// offsetY: pixel-offset of grid from bottom
+// x & y: traditional plotting
+//        i.e: y  ____________________
+//             4 |___|___|___|___|___|
+//             3 |___|___|___|___|___|
+//             2 |___|___|___|___|___|
+//             1 |___|___|___|___|___|
+//             0 |___|___|___|___|___|
+//                 0   1   2   3   4   x
+var GRID = {
+    width: 5,
+    height: 5,
+    offsetY: 68,
+    x: [],
+    y: []
+};
+
+// Set x coordinates for each cell in the grid
+for (var i = 0; i < GRID.width; i++)
+  GRID.x[i] = i * CELL.width;
+
+// Find y coordinate of the bottom of the grid
+GRID.bottom = CELL.height * (GRID.height - 1) + GRID.offsetY;
+
+// Set y coordinates for each cell in the grid
+for (var i = 0; i < GRID.height; i++)
+    GRID.y[i] = GRID.bottom - (i * CELL.height);
